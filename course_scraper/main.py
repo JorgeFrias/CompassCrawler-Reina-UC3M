31c3338713fc31eafdc9f5cfe097f2ab7afc537c
Jorge Frias <100317118@alumnos.uc3m.es>
2018-02-06 11:38:37 +0100

    Avanzando.

--- course_scraper/main.py
+++ course_scraper/main.py
@@ -1,2 +1,12 @@
 from scrapy import cmdline
-cmdline.execute("scrapy crawl spider_courses".split())
+import sys
+
+command = ("scrapy crawl --nolog spider_courses")
+
+if sys.argv[1] == 'a':
+    command = ("scrapy crawl --nolog spider_courses")
+
+if sys.argv[1] == 'b':
+    command = ("scrapy crawl  spider_courses -o data.csv -t csv")
+
+cmdline.execute(command.split())
